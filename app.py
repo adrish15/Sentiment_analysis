01a3ea85c54063c08595e0f7d3bb4404b8c73576
adrish15 <61633697+adrish15@users.noreply.github.com>
2022-03-21 14:38:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,56 +2,16 @@ import streamlit as st
 import tensorflow as tf
 import tensorflow.keras as keras
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
-imdb_reviews=pd.read_csv("imdb_reviews.csv")
-test_reviews=pd.read_csv("test_reviews.csv")
-word_index=pd.read_csv("word_indexes.csv")
-word_index=dict(zip(word_index.Words,word_index.Indexes))
-
-word_index["<PAD>"]=0
-word_index["<START"]=1
-word_index["<UNK>"]=2
-word_index["<UNUSED>"]=3
-
-def review_encoder(text):
-  arr=[word_index[word] for word in text]
-  return arr
-train_data,train_labels=imdb_reviews['Reviews'],imdb_reviews['Sentiment']
-test_data, test_labels=test_reviews['Reviews'],test_reviews['Sentiment']
-
-train_data=train_data.apply(lambda review:review.split())
-test_data=test_data.apply(lambda review:review.split())
-
-train_data=train_data.apply(review_encoder)
-test_data=test_data.apply(review_encoder)
-
+from keras.models import load_model
+model = load_model('my_model.h5')
 
 def encode_sentiments(x):
   if x=='positive':
     return 1
   else:
     return 0
-
-train_labels=train_labels.apply(encode_sentiments)
-test_labels=test_labels.apply(encode_sentiments)
-
-
-train_data=keras.preprocessing.sequence.pad_sequences(train_data,value=word_index["<PAD>"],padding='post',maxlen=500)
-test_data=keras.preprocessing.sequence.pad_sequences(test_data,value=word_index["<PAD>"],padding='post',maxlen=500)
-
-model=keras.Sequential([keras.layers.Embedding(10000,16,input_length=500),
-                        keras.layers.GlobalAveragePooling1D(),
-                        keras.layers.Dense(16,activation='relu'),
-                        keras.layers.Dense(1,activation='sigmoid')])
-
-model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
-
-
-history=model.fit(train_data,train_labels,epochs=30,batch_size=512,validation_data=(test_data,test_labels))
-index=np.random.randint(1,1000)
 
 st.title("Welcome to Sentiment analyzer")
 user_review = st.text_input("Your feedback", key="text")
