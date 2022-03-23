de665c5d9c22b0d4424305c1b7571b982a553347
adrish15 <61633697+adrish15@users.noreply.github.com>
2022-03-23 22:33:57 +0530

    Add files via upload

---
+++ adrish.py
@@ -0,0 +1,28 @@
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+import pickle
+# Load model
+model = load_model('/content/drive/MyDrive/Colab Data files/my_model.h5')
+#loading tokenizer
+with open('/content/drive/MyDrive/Colab Data files/tokenizer.pickle', 'rb') as tokenizer_sav:
+    tokenizer = pickle.load(tokenizer_sav)
+    
+def predict_class(text):
+    
+    sentiment_classes = ['Negative', 'Neutral', 'Positive']
+    max_len=600
+    
+   
+    xt = tokenizer.texts_to_sequences(text)
+
+    xt = pad_sequences(xt, padding='post', maxlen=max_len)
+    # Do the prediction using the loaded model
+    yt = model.predict(xt).argmax(axis=1)
+
+    print('The predicted sentiment is', sentiment_classes[yt[0]])
+    
+predict_class(["My experience so far has been fantastic"])
+
+predict_class(["the movie was bad"])
+
+predict_class(["take an umbrella"])
