3838fb26074f80d4409b18de9e5aeae5f748b874
adrish15 <61633697+adrish15@users.noreply.github.com>
2022-03-24 13:36:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,38 +31,10 @@ def review_encoder(text):
   return arr
 add_selectbox = st.sidebar.selectbox(
     'Feedback category',
-    ('Movies', 'Books','Twitter Analysis')
+    ('Twitter Analysis')
 )
-#st.text_input("Your feedback", key="text")
 
-left_column, right_column = st.columns(2)
-if add_selectbox=='Movies' or 'Books':
-	st.title("Welcome to Sentiment analyzer")
-	user_review = st.text_input("Your feedback", key="text")
-	user_review=user_review.lower()
-	user_review = user_review.split()
-	user_review = review_encoder(user_review)
-	user_review=np.array([user_review])
-	user_review=keras.preprocessing.sequence.pad_sequences(user_review,value=word_index["<PAD>"],padding='post',maxlen=500)
-	'''
-	add_selectbox = st.sidebar.selectbox(
-		'Feedback category',
-		('Movies', 'Books','Twitter Analysis')
-	)
-	#st.text_input("Your feedback", key="text")
-
-	left_column, right_column = st.columns(2)
-	if add_selectbox=='Movies' or 'Books':
-	# You can use a column just like st.sidebar:
-	'''
-	if left_column.button('sentiment!'):
-
-		if (model.predict(user_review)>0.5).astype("int32"):
-			st.write('positive sentiment')
-		else:
-			st.write("negative sentiment")
-
-elif add_selectbox=='Twitter_Analysis':
+if add_selectbox=='Twitter_Analysis':
   st.wwrite("hi welcome to twitter analysis")
   load_dotenv()
 
