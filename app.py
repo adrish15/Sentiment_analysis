6cf32bacd5e4032562a5acef4909a71950fdf5cc
adrish15 <61633697+adrish15@users.noreply.github.com>
2022-03-25 00:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ word_index["<UNUSED>"]=3
 def review_encoder(text):
   arr=[word_index[word] for word in text]
   return arr
-add_selectbox = st.sidebar.radio(
+add_selectbox = st.sidebar.selectbox(
     'Feedback category',
     ('Movies', 'Airline senitment','Twitter Analysis')
 )
