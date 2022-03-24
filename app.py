a9f74478ee9c116c6cfa48e1ae5c97d12401121b
adrish15 <61633697+adrish15@users.noreply.github.com>
2022-03-24 16:56:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,16 +44,7 @@ if add_selectbox=='Movies' or 'Books':
 	user_review = review_encoder(user_review)
 	user_review=np.array([user_review])
 	user_review=keras.preprocessing.sequence.pad_sequences(user_review,value=word_index["<PAD>"],padding='post',maxlen=500)
-	'''
-	add_selectbox = st.sidebar.selectbox(
-		'Feedback category',
-		('Movies', 'Books','Twitter Analysis')
-	)
-	#st.text_input("Your feedback", key="text")
-	left_column, right_column = st.columns(2)
-	if add_selectbox=='Movies' or 'Books':
-	# You can use a column just like st.sidebar:
-	'''
+	
 	if left_column.button('sentiment!'):
 
 		if (model.predict(user_review)>0.5).astype("int32"):
